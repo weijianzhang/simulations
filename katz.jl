1fd56e16edc928f1e891406830eb6ba530ea5c7f
Weijian Zhang <weijian.z@hotmail.com>
2017-09-25 14:30:15 +0100

    add jmlr author network data

--- katz.jl
+++ katz.jl
@@ -14,5 +14,10 @@ end
 
 rating = katz_centrality(eg)
 
-# show top 10 authors
-sort(rating, by = x -> x[2], rev = true)[1:10]
+sorted_authors = sort(rating, by = x -> x[2], rev = true)
+
+println("Top 10 rating authors")
+println(sorted_authors[1:10])
+
+println("Button 10 rating authors")
+println(sorted_authors[end-10:end])
